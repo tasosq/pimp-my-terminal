c5a940683fd108de086ebacd3bfe9dfa7505aecb
Tasos Georgousis <anas.georgou@gmail.com>
2024-03-23 21:07:38 +0200

    Fix the name of the script that download-dependencies points you towards after it is run

--- download-dependencies.ps1
+++ download-dependencies.ps1
@@ -8,7 +8,7 @@ Write-Host "Installing oh-my-posh..."
 winget install oh-my-posh -s msstore --accept-package-agreements --accept-source-agreements
 winget install JanDeDobbeleer.OhMyPosh -s winget --accept-package-agreements --accept-source-agreements
 
-Write-Host "Please exit Windows PowerShell and Launch Windows Terminal from Start Menu. Then Run Install_Step2 to continue the setup.";
+Write-Host "Please exit Windows PowerShell and Launch Windows Terminal from Start Menu. Then Run setup-shell-terminal to continue the setup.";
 
 Write-Host  'Press any key to continue..';
 $null = $Host.UI.RawUI.ReadKey('NoEcho,IncludeKeyDown');
